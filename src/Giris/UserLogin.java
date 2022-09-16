8a783dd08a7374be1cc1fce80cc945f9ab1ac5a3
Alicukur <alicukur.yzlm@gmail.com>
2022-09-17 00:29:40 +0300

    PatikaOdevVePratikler

---
+++ src/Giris/UserLogin.java
@@ -0,0 +1,52 @@
+package Giris;
+import java.util.Scanner;
+
+
+public class UserLogin {
+    public static void main(String[] args) {
+
+        // Degiskenleri tanımladık
+        String userName , password , secim;
+
+        // Scanner sınıfından yararlandık kullanıcı adı ve  password verisini almak için
+        Scanner veri = new Scanner(System.in);
+        System.out.print("Kullanıcı adınızı giriniz : ");
+        userName = veri.nextLine();
+        System.out.print("Kullanıcı adınızı giriniz : ");
+        password = veri.nextLine();
+
+
+
+        // Kosulu olusturduk .
+        if (userName.equals("Lazzali") && password.equals("ali1234"))
+        {
+            System.out.println("Hosgeldiniz");
+        }
+        else{
+
+            // İkinci bir durum oluşturuldu şifre unutulduysa static olusturduk .
+
+            System.out.println("Kullanıcı bilgileri yalnıs, Sıfrenizi yenilemek ister misiniz ? \"yes\" or \"no\" " );
+            secim = veri.nextLine();
+
+            // Kullanıcı sifresini yenilemek isterse ;
+            if (secim.equals("yes")) {
+                String newpassword;
+                System.out.println("Yeni sifrenizi giriniz ");
+                newpassword = veri.nextLine();
+                if (password.equals("ali1234") != newpassword.equals("ali345")) {
+                    System.out.println("sifre değiştirildi");
+                }
+                else{
+                    System.out.println("Sifre yenilenemedi eski sifrenizi giriyorsunuz .");
+                }
+
+            }
+            // kullanıcı sifresini yenilemek istemezse ;
+                else if (secim.equals("no"))
+                {
+                    System.out.println("Bi basaramadın login olmayı");
+                }
+        }
+    }
+}
