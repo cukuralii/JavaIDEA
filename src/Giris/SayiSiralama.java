dc990d1b94019ce3e6064669e9f953e0e443ea73
Alicukur <alicukur.yzlm@gmail.com>
2022-09-19 02:11:54 +0300

    PatikaOdevVePratikler

--- src/Giris/SayiSiralama.java
+++ src/Giris/SayiSiralama.java
@@ -15,17 +15,26 @@ public class SayiSiralama {
         System.out.print("Ucuncu sayi : " );
         sayi3= verilendeger.nextInt();
 
-        if (sayi>sayi2 && sayi2>sayi3)
+        if (sayi>sayi2 && sayi>sayi3)
         {
-            System.out.println("Sayılarımızın büyükten kücüge sıralanısı : "  + sayi + ">"+sayi2 +">"+sayi3 );
+            if (sayi2>sayi3) {
+                System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi + ">" + sayi2 + ">" + sayi3);
+            }
+            else {System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi + ">" + sayi3 + ">" + sayi2);};
         }
-        else if (sayi2>sayi && sayi>sayi3)
+        if (sayi2>sayi && sayi2>sayi3)
         {
-            System.out.println("Sayılarımızın büyükten kücüge sıralanısı : "  + sayi2 + ">"+sayi +">"+sayi3 );
+            if (sayi>sayi3) {
+                System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi2 + ">" + sayi + ">" + sayi3);
+            }
+            else {System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi2 + ">" + sayi3 + ">" + sayi);};
         }
-        else if (sayi3>=sayi2 && sayi2>sayi)
+        if (sayi3>sayi2 && sayi3>sayi)
         {
-            System.out.println("Sayılarımızın büyükten kücüge sıralanısı : "  + sayi3 + ">"+sayi2 +">"+sayi );
+            if (sayi>sayi2) {
+                System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi3 + ">" + sayi + ">" + sayi2);
+            }
+            else {System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi3 + ">" + sayi2 + ">" + sayi);};
         }
 
     }
