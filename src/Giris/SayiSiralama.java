7b1d890e65e7ade28cee26e3e7d7e21d04c2c7a1
Alicukur <alicukur.yzlm@gmail.com>
2022-09-19 02:15:39 +0300

    PatikaOdevVePratikler

--- src/Giris/SayiSiralama.java
+++ src/Giris/SayiSiralama.java
@@ -6,6 +6,7 @@ public class SayiSiralama {
     public static void main(String[] args) {
         System.out.println("Lütfen sayıları giriniz :) ");
 
+        // VERİLERİMİZİ TANIMLADIK.
         Scanner verilendeger = new Scanner(System.in);
         int sayi ,sayi2 ,sayi3 ;
         System.out.print("Birinci sayi : " );
@@ -15,26 +16,27 @@ public class SayiSiralama {
         System.out.print("Ucuncu sayi : " );
         sayi3= verilendeger.nextInt();
 
+         // KOSULLARIMIZI OLUSTURDUK
         if (sayi>sayi2 && sayi>sayi3)
         {
             if (sayi2>sayi3) {
                 System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi + ">" + sayi2 + ">" + sayi3);
             }
-            else {System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi + ">" + sayi3 + ">" + sayi2);};
+            else {System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi + ">" + sayi3 + ">" + sayi2);}
         }
         if (sayi2>sayi && sayi2>sayi3)
         {
             if (sayi>sayi3) {
                 System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi2 + ">" + sayi + ">" + sayi3);
             }
-            else {System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi2 + ">" + sayi3 + ">" + sayi);};
+            else {System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi2 + ">" + sayi3 + ">" + sayi);}
         }
         if (sayi3>sayi2 && sayi3>sayi)
         {
             if (sayi>sayi2) {
                 System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi3 + ">" + sayi + ">" + sayi2);
             }
-            else {System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi3 + ">" + sayi2 + ">" + sayi);};
+            else {System.out.println("Sayılarımızın büyükten kücüge sıralanısı : " + sayi3 + ">" + sayi2 + ">" + sayi);}
         }
 
     }
