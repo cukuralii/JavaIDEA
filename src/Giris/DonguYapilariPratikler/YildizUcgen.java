cafd56d79027d0dbbd484e720ff4e956afa7b886
Alicukur <alicukur.yzlm@gmail.com>
2022-09-20 22:27:18 +0300

    PatikaOdevVePratikler

---
+++ src/Giris/DonguYapilariPratikler/YildizUcgen.java
@@ -0,0 +1,30 @@
+package Giris.DonguYapilariPratikler;
+import java.util.Scanner;
+
+public class YildizUcgen {
+    public static void main(String[] args) {
+        Scanner veri = new Scanner(System.in);
+        System.out.print("Enter any number:     ");
+        int a = veri.nextInt();
+        for (int i = 1; i <= a; i++) {
+            for (int j = 0; j < (a - i); j++) {
+                System.out.print(" ");
+            }
+            for (int k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+        for (int i=0;i<=a;i++){
+            for (int j=a; j>(a-i);j--){
+                System.out.print(" ");
+            }
+            for (int k=1; k<=(2*(a-i)-1);k++){
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+
+
+    }
+}
