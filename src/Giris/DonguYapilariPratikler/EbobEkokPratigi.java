5c8a7e5a319a0864d77b0ab4a1b709e2db3bcf89
Alicukur <alicukur.yzlm@gmail.com>
2022-09-21 01:14:31 +0300

    PatikaOdevVePratikler

---
+++ src/Giris/DonguYapilariPratikler/EbobEkokPratigi.java
@@ -0,0 +1,32 @@
+package Giris.DonguYapilariPratikler;
+import  java.util.Scanner;
+public class EbobEkokPratigi {
+    public static void main(String[] args) {
+        int sayi1, sayi2, kucukSayi;
+        int ebob = 1;
+
+        Scanner girdi1 = new Scanner(System.in);
+        System.out.print("Birinci Sayiyi Giriniz : ");
+        sayi1 = girdi1.nextInt();
+
+        Scanner girdi2 = new Scanner(System.in);
+        System.out.print("Ikinci Sayiyi Giriniz : ");
+        sayi2 = girdi2.nextInt();
+
+        if (sayi1 > sayi2) {
+            kucukSayi = sayi2;
+        } else {
+            kucukSayi = sayi1;
+        }
+        int x = kucukSayi;
+        while (x >= 1) {
+            if ((sayi1 % x == 0) && (sayi2 % x == 0)) {
+                ebob = x;
+                System.out.println("Ebob : " + ebob);
+                break;
+            }
+            x--;
+        }
+        System.out.print("Ekok : " + (sayi1 * sayi2) / ebob);
+    }
+}
