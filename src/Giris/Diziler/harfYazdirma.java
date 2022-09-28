f9ad6ecfd4a257d7bb6e79add27d02d863b106c6
Alicukur <alicukur.yzlm@gmail.com>
2022-09-28 19:19:53 +0300

    PatikaOdevVePratikler

--- src/Giris/Diziler/harfYazdirma.java
+++ src/Giris/Diziler/harfYazdirma.java
@@ -4,6 +4,8 @@ public class harfYazdirma {
     public static void main(String[] args) {
         String [][] letter = new String[7][4];
 
+        // birden fazla boyutlu dizilerde ilk satır sonra sütün içerisinde dönüyoruz . Satır sayısı kesinlikle
+        // verilmesi gerekiyor.
         for (int i = 0; i < letter.length ; i++) {
             for (int j = 0; j <letter[i].length ; j++) {
                 if (i==0 || i==6 || i==3 )
